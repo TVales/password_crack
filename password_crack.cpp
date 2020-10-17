3832350a81afc6d8071cb95f55aa99be23a03fdd
Tvales <timv96@vt.edu>
2020-10-16 23:14:20 -0700

    first commit

---
+++ password_crack.cpp
@@ -0,0 +1,32 @@
+/*
+Password Cracking Algorithm 
+
+Password Type: Alpha numeric 
+               numeric (*)
+               All characters 
+               Words concatenated
+
+Password Length: 4 - iphone number pad (*)
+                 5 - test how much adding 1 increases password crack time
+                 10 - medium strength password 
+                 15 - strong password strength
+
+Algorithms: Brute Force 
+            Multithreading?  
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+}
+
+//generate password using random number generator
+//all numbers, length 4 
+int generate_num_pw(int seed, int length) {
+    
+}
+
+//brute force algorithm 
